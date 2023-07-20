6b14eb4cd8e87ef14e09a20d5c8ccf9697b576e7
Theo Fandrich <tf@harmony.one>
2023-07-19 18:53:27 -0700

    updated with manual date, fixed branding/wording

--- src/components/WalletModal/PrivacyPolicyNotice.tsx
+++ src/components/WalletModal/PrivacyPolicyNotice.tsx
@@ -16,18 +16,20 @@ const LAST_UPDATED_DATE = '11.17.22'
 export default function PrivacyPolicyNotice() {
   return (
     <ThemedText.Caption color="textSecondary">
-      <Trans>By connecting a wallet, you agree to Uniswap Labs&apos;</Trans>{' '}
-      <StyledLink href="https://uniswap.org/terms-of-service/">
+      <Trans>By connecting a wallet, you agree to Protofire&apos;</Trans>{' '}
+      <StyledLink href="https://www.notion.so/Terms-of-Service-452ba9ee953e460983ed7e5558dc0397">
         <Trans>Terms of Service</Trans>{' '}
       </StyledLink>
       <Trans>and consent to its</Trans>{' '}
-      <StyledLink href="https://uniswap.org/privacy-policy">
+      <StyledLink href="https://harmonyone.notion.site/Privacy-Policy-3a2eef1087c14a5a84eec272ceb76f70?pvs=4">
         <Trans>Privacy Policy.</Trans>
       </StyledLink>
       <LastUpdatedText>
         {' ('}
         <Trans>Last Updated</Trans>
-        {` ${LAST_UPDATED_DATE})`}
+        {/* {` ${LAST_UPDATED_DATE})`} */}
+        {` 7.19.23)`}
+
       </LastUpdatedText>
     </ThemedText.Caption>
   )
