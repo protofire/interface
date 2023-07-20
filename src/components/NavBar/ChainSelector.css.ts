9b86ffe790efce879518d95987bb48368f1da9dc
Theo Fandrich <tf@harmony.one>
2023-07-19 19:38:43 -0700

    updated to remove pointer and background on network logo

--- src/components/NavBar/ChainSelector.css.ts
+++ src/components/NavBar/ChainSelector.css.ts
@@ -4,11 +4,11 @@ import { lightGrayOverlayOnHover } from 'nft/css/common.css'
 import { sprinkles } from '../../nft/css/sprinkles.css'
 
 export const ChainSelector = style([
-  lightGrayOverlayOnHover,
+  // lightGrayOverlayOnHover,
   sprinkles({
     borderRadius: '8',
     height: '40',
-    cursor: 'pointer',
+    cursor: 'default',
     border: 'none',
     color: 'textPrimary',
     background: 'none',
