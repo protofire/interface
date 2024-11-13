ba56b9ee7ca485a192b5f8ce7b2c6f1e6d85ffd9
msvstj <savastijonok@gmail.com>
2024-11-13 17:42:29 +0200

    feat: add token list for zero

--- apps/web/src/constants/lists.ts
+++ apps/web/src/constants/lists.ts
@@ -1,7 +1,10 @@
 // Lists we use as fallbacks on chains that our backend doesn't support
 const COINGECKO_AVAX_LIST = 'https://tokens.coingecko.com/avalanche/all.json'
-const ABSTRACT_LISt = 'https://api.testnets.relay.link/tokenlist?chainId=11124'
+const ABSTRACT_LIST = 'https://api.testnets.relay.link/tokenlist?chainId=11124'
+
+// For faster load using URL with specified chainID param
+const ZERO_LIST = 'https://api.relay.link/tokenlist?chainId=543210'
 
 export const DEFAULT_INACTIVE_LIST_URLS: string[] = [COINGECKO_AVAX_LIST]
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [ABSTRACT_LISt]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [ABSTRACT_LIST, ZERO_LIST]
 export const DEFAULT_LIST_OF_LISTS: string[] = [...DEFAULT_ACTIVE_LIST_URLS]
