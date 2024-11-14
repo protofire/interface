f90390111c7312fe4457280bc61d77b93ad8c451
Ted Palmer <etpalmer17@gmail.com>
2024-11-14 12:22:30 -0500

    Remove homepage

--- apps/web/src/components/NavBar/CompanyMenu/index.tsx
+++ apps/web/src/components/NavBar/CompanyMenu/index.tsx
@@ -49,8 +49,7 @@ export function CompanyMenu() {
 
   const handleLogoClick = useCallback(() => {
     navigate({
-      pathname: '/',
-      search: '?intro=true',
+      pathname: '/swap',
     })
   }, [navigate])
   const isTouchDevice = useIsTouchDevice()
