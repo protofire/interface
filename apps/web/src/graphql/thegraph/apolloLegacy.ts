66ed542d822edd013a2d639a33f7f4cbbf90afe1
Leoni Mella <leoni.mella@gmail.com>
2024-12-05 17:01:11 -0300

    feat: add cyber v3-subgraph url
    - fix: bob v3-subgraph url

--- apps/web/src/graphql/thegraph/apolloLegacy.ts
+++ apps/web/src/graphql/thegraph/apolloLegacy.ts
@@ -10,7 +10,8 @@ const CHAIN_SUBGRAPH_URL: Record<number, string> = {
   [UniverseChainId.AbstractTestnet]:
     'https://graph-node.internal.reservoir.tools/subgraphs/name/absctract-testnet/v3-subgraph',
   [UniverseChainId.Zero]: 'https://graph-node.internal.reservoir.tools/subgraphs/name/zero/v3-subgraph',
-  [UniverseChainId.BOB]: 'https://graph-node.internal.reservoir.tools/subgraphs/bob/zero/v3-subgraph',
+  [UniverseChainId.BOB]: 'https://graph-node.internal.reservoir.tools/subgraphs/name/bob/v3-subgraph',
+  [UniverseChainId.CYBER]: 'https://graph-node.internal.reservoir.tools/subgraphs/name/cyber/v3-subgraph',
 }
 
 const httpLink = new HttpLink({ uri: CHAIN_SUBGRAPH_URL[UniverseChainId.AbstractTestnet] })
