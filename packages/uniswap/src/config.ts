f119765555db775d40b99ac5a163562b14d95e60
msvstj <savastijonok@gmail.com>
2025-01-28 15:00:18 +0200

    feat: remove dynamic chain support

--- packages/uniswap/src/config.ts
+++ packages/uniswap/src/config.ts
@@ -108,11 +108,6 @@ const _config: Config = {
     QUICKNODE_MAINNET_RPC_URL,
   tradingApiKey: process.env.TRADING_API_KEY || TRADING_API_KEY,
   firebaseAppCheckDebugToken: process.env.FIREBASE_APP_CHECK_DEBUG_TOKEN || FIREBASE_APP_CHECK_DEBUG_TOKEN,
-  webSupportedChains: process.env.REACT_APP_WEB_SUPPORTED_CHAINS
-    ? process.env.REACT_APP_WEB_SUPPORTED_CHAINS.split(',')
-        .map((id) => Number(id) as UniverseChainId)
-        .filter((id): id is UniverseChainId => !!id)
-    : undefined,
   inkRPCURL: process.env.REACT_APP_INK_RPC_URL,
 }
 
