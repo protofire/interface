ef557458aa229e2612a8aeb2ea4df174cc39df8b
Denis Smolonski <denissmolonski@gmail.com>
2025-04-16 08:13:53 +0200

    feat: add anime mainnet

--- apps/web/src/constants/lists.ts
+++ apps/web/src/constants/lists.ts
@@ -3,6 +3,8 @@ const DEFAULT_TOKEN_LIST =
 
 const ZERO_LIST = 'https://raw.githubusercontent.com/protofire/token-list/refs/heads/main/networks/zero.json'
 
+const ANIME_LIST = 'https://raw.githubusercontent.com/protofire/token-list/refs/heads/main/networks/anime.json'
+
 export const DEFAULT_INACTIVE_LIST_URLS: string[] = []
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [DEFAULT_TOKEN_LIST, ZERO_LIST]
-export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST, ZERO_LIST]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [DEFAULT_TOKEN_LIST, ZERO_LIST, ANIME_LIST]
+export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST, ZERO_LIST, ANIME_LIST]
