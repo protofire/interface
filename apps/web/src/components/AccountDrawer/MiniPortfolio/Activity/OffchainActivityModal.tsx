843211bc6a86d5187d68a24b8201cfec5a6f9b89
msvstj <savastijonok@gmail.com>
2025-09-02 12:18:44 +0300

    feat: supported networks in dropdown

--- apps/web/src/components/AccountDrawer/MiniPortfolio/Activity/OffchainActivityModal.tsx
+++ apps/web/src/components/AccountDrawer/MiniPortfolio/Activity/OffchainActivityModal.tsx
@@ -224,7 +224,7 @@ export function OrderContent({
     <Column>
       <Row gap="md">
         <PortfolioLogo
-          chainId={amounts?.inputAmount.currency.chainId ?? UniverseChainId.AbstractTestnet}
+          chainId={amounts?.inputAmount.currency.chainId ?? UniverseChainId.AbstractMainnet}
           currencies={currencies}
           images={[logos?.inputLogo, logos?.outputLogo]}
         />
