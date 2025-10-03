9a4036c09bf92dba23d81afb7198840fc2015cba
msvstj <savastijonok@gmail.com>
2025-10-03 13:34:27 +0300

    feat: add zero link

--- apps/web/src/constants/links.ts
+++ apps/web/src/constants/links.ts
@@ -5,6 +5,8 @@ export const getInfoV2Link = (chainId: string | number) => {
   switch (chainId) {
     case 69000:
       return isProd ? 'https://v2-info-anime.sakuraswap.com' : 'https://v2-info-anime.staging.sakuraswap.com'
+    case 543210:
+      return isProd ? 'https://v2-info-zero.sakuraswap.com' : 'https://v2-info-zero.staging.sakuraswap.com'
     default:
       return isProd ? 'https://v2-info.sakuraswap.com' : 'https://v2-info.staging.sakuraswap.com'
   }
