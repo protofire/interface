31c7eaa51080b22f7fa1491e0f43dcb6ac080d0e
msvstj <savastijonok@gmail.com>
2025-11-04 19:22:18 +0200

    chore: add URL to DAOImage

--- apps/web/src/pages/Landing/sections/Footer.tsx
+++ apps/web/src/pages/Landing/sections/Footer.tsx
@@ -101,7 +101,7 @@ export function Footer() {
         <Flex height="100%" gap="$spacing60">
           <Flex $md={{ display: 'none' }} flexDirection="column" gap="$spacing16">
             <Socials iconSize={SOCIAL_ICONS_SIZE} />
-            <Anchor>
+            <Anchor href="https://gov.uniswap.org/t/rfc-deploy-uniswap-v3-on-shape/25163" target="_blank">
               <DAOLogo src={isDarkMode ? DAODarkMode : DAOLightMode} alt="DAO Logo" />
             </Anchor>
           </Flex>
@@ -119,7 +119,7 @@ export function Footer() {
         </Flex>
         <Flex $md={{ display: 'flex' }} display="none" flexDirection="column" gap="$spacing16">
           <Socials iconSize={SOCIAL_ICONS_SIZE} />
-          <Anchor>
+          <Anchor href="https://gov.uniswap.org/t/rfc-deploy-uniswap-v3-on-shape/25163" target="_blank">
             <DAOLogo src={isDarkMode ? DAODarkMode : DAOLightMode} alt="DAO Logo" />
           </Anchor>
         </Flex>
@@ -132,7 +132,7 @@ export function Footer() {
         width="100%"
         justifyContent="space-between"
       >
-        <Text variant="body3">2025 ShapeSwap</Text>
+        <Text variant="body3">2025 FlowSwap</Text>
         <Anchor style={{ textDecoration: 'none' }} href="https://protofire.io" target="_blank">
           <Text variant="body3" style={{ display: 'flex', alignItems: 'center' }}>
             Supported by <CompanyIcon style={{ marginLeft: 4, marginRight: 4 }} />
