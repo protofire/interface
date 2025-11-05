f23011cea371a1555b4d2dc49ef27f3c1df80b84
msvstj <savastijonok@gmail.com>
2025-11-05 13:42:35 +0200

    fix: footer text

--- apps/web/src/pages/Landing/sections/Footer.tsx
+++ apps/web/src/pages/Landing/sections/Footer.tsx
@@ -132,7 +132,7 @@ export function Footer() {
         width="100%"
         justifyContent="space-between"
       >
-        <Text variant="body3">2025 FlowSwap</Text>
+        <Text variant="body3">2025 ShapeSwap</Text>
         <Anchor style={{ textDecoration: 'none' }} href="https://protofire.io" target="_blank">
           <Text variant="body3" style={{ display: 'flex', alignItems: 'center' }}>
             Supported by <CompanyIcon style={{ marginLeft: 4, marginRight: 4 }} />
