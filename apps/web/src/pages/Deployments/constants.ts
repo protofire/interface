9c57082cb147a2b5b22db0dbc5ad941c4f0b68f9
msvstj <savastijonok@gmail.com>
2025-11-05 12:47:35 +0200

    refactor: hardcoded chain ID in DAO links

--- apps/web/src/pages/Deployments/constants.ts
+++ apps/web/src/pages/Deployments/constants.ts
@@ -1,3 +1,5 @@
+import { UniverseChainId } from "uniswap/src/types/chains";
+
 export const DAO_LINKS = {
-    360: "https://gov.uniswap.org/t/rfc-deploy-uniswap-v3-on-shape/25163/6"
+    [UniverseChainId.SHAPE]: "https://gov.uniswap.org/t/rfc-deploy-uniswap-v3-on-shape/25163/6"
 }
