7c8a1c4d8b74013bb3963e11935b753f8e0adf90
msvstj <savastijonok@gmail.com>
2025-11-05 12:31:32 +0200

    feat: apply badges to dao approved chains + link deployments in the menu

--- apps/web/src/components/NavBar/CompanyMenu/Content.tsx
+++ apps/web/src/components/NavBar/CompanyMenu/Content.tsx
@@ -21,7 +21,10 @@ export const useMenuContent = (): MenuSection[] => {
     // TODO: add translations once content is confirmed
     {
       title: 'Help',
-      items: [{ label: 'Contact Support', href: 'https://swap-support.protofire.io/' }],
+      items: [
+        { label: 'Contact Support', href: 'https://swap-support.protofire.io/' },
+        { label: 'Contract Deployments', href: '/deployments' }
+      ],
     },
     {
       title: 'Company',
---
+++ apps/web/src/pages/Deployments/constants.ts
@@ -0,0 +1,6 @@
+import { UniverseChainId } from "uniswap/src/types/chains";
+
+export const DAO_LINKS = {
+    [UniverseChainId.AbstractMainnet]: "https://gov.uniswap.org/t/rfc-deploy-uniswap-v3-on-abstract/25206/3",
+    [UniverseChainId.Zero]: "https://gov.uniswap.org/t/zero-application-for-canonical-uniswap-v3-deployment/24893/2"
+}
