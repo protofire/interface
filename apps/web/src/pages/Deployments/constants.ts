a8c4d2fded1eb712fa494dc2b148a552d2d75a43
msvstj <savastijonok@gmail.com>
2025-11-05 12:47:04 +0200

    chore: add link to deployments, change hardcoded chain ID in DAO links

--- apps/web/src/pages/Deployments/constants.ts
+++ apps/web/src/pages/Deployments/constants.ts
@@ -1,3 +1,5 @@
+import { UniverseChainId } from "uniswap/src/types/chains";
+
 export const DAO_LINKS = {
-    747: "https://gov.uniswap.org/t/rfc-flow-application-for-canonical-uniswap-v3-deployment/25876"
+    [UniverseChainId.FlowMainnet]: "https://gov.uniswap.org/t/rfc-flow-application-for-canonical-uniswap-v3-deployment/25876"
 }
