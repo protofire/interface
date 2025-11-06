b79e99381d52cff614f715f14d167bd5fbc8bdfc
msvstj <savastijonok@gmail.com>
2025-11-06 15:28:54 +0200

    feat: update token list

--- apps/web/src/constants/lists.ts
+++ apps/web/src/constants/lists.ts
@@ -1,4 +1,4 @@
-const DEFAULT_TOKEN_LIST = 'https://cyan-legislative-leech-117.mypinata.cloud/ipfs/QmWFoYN4bFDB3g42pp8w9qgfS8gYVbo6wmeoCWiDcXFmcZ'
+const DEFAULT_TOKEN_LIST = 'https://cyan-legislative-leech-117.mypinata.cloud/ipfs/QmXPqqYJsTLvSrkLZAZnzVsfbCyZAopzE91ALsjnPSsbWF'
 export const DEFAULT_INACTIVE_LIST_URLS: string[] = []
 export const DEFAULT_ACTIVE_LIST_URLS: string[] = [DEFAULT_TOKEN_LIST]
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST]
