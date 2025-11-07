5205fd644a9f08280c099588a0e9ffa2761207f9
Leoni Mella <leoni.mella@gmail.com>
2025-11-07 15:47:21 -0300

    chore: change help menu ordering

--- apps/web/src/components/NavBar/CompanyMenu/Content.tsx
+++ apps/web/src/components/NavBar/CompanyMenu/Content.tsx
@@ -21,7 +21,10 @@ export const useMenuContent = (): MenuSection[] => {
     // TODO: add translations once content is confirmed
     {
       title: 'Help',
-      items: [{ label: 'Contact Support', href: 'https://swap-support.protofire.io/' }],
+      items: [
+        { label: 'Deployed Contracts', href: '/deployments' },
+        { label: 'Contact Support', href: 'https://swap-support.protofire.io/' },
+      ],
     },
     {
       title: 'Company',
