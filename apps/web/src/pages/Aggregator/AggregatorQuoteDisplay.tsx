5c392c80966776e631d779bb4818b44ea961c38d
msvstj <savastijonok@gmail.com>
2025-11-19 14:12:00 +0200

    feat: add price impact for comparison

--- apps/web/src/pages/Aggregator/AggregatorQuoteDisplay.tsx
+++ apps/web/src/pages/Aggregator/AggregatorQuoteDisplay.tsx
@@ -150,8 +150,7 @@ export function AggregatorQuoteDisplay({ quote, loading, error, slippage, exchan
           <Value>{Number((slippage * 100).toFixed(2))}%</Value>
         </QuoteRow>
       )}
-      {/* TODO: reenable */}
-      {/* {priceImpact && (
+      {priceImpact && (
         <QuoteRow>
           <Label>Price Impact:</Label>
           <Value>
@@ -164,7 +163,7 @@ export function AggregatorQuoteDisplay({ quote, loading, error, slippage, exchan
             })()}
           </Value>
         </QuoteRow>
-      )} */}
+      )}
       <QuoteRow>
         <Label>Receive at least:</Label>
         <Value>{minAmountFormatted} {outputTokenSymbol}</Value>
