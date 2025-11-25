cef0b40a3b0b3f62f780eb4033429e24cb6ffa0e
Leoni Mella <leoni.mella@gmail.com>
2025-11-25 13:23:01 -0300

    chore: update info v2 & v3 links

--- apps/web/src/components/NavBar/Tabs/TabsContent.tsx
+++ apps/web/src/components/NavBar/Tabs/TabsContent.tsx
@@ -84,7 +84,7 @@ export const useTabsContent = (props?: { includeNftsLink?: boolean }): TabsSecti
     },
     {
       title: t('common.explore'),
-      href: `https://info.staging.stable-temp.w3us.site.xyz/home`,
+      href: `https://info.staging.swap.stable.xyz/home`,
       isActive: pathname.startsWith('/explore') || pathname.startsWith('/nfts'),
       icon: <ExternalLink size="$icon.16" color="$neutral2" />,
       internal: false,
@@ -118,7 +118,7 @@ export const useTabsContent = (props?: { includeNftsLink?: boolean }): TabsSecti
             {
               label: 'V3 Analytics',
               quickKey: 'T',
-              href: `https://info.staging.stable-temp.w3us.site.xyz/home`,
+              href: `https://info.staging.swap.stable.xyz/home`,
               internal: false,
               blank: true,
             },
@@ -127,7 +127,7 @@ export const useTabsContent = (props?: { includeNftsLink?: boolean }): TabsSecti
                   {
                     label: 'V2 Analytics',
                     quickKey: 'P',
-                    href: `https://v2-info.staging.stable-temp.w3us.site.xyz`,
+                    href: `https://v2-info.staging.swap.stable.xyz`,
                     internal: false,
                     blank: true,
                   },
