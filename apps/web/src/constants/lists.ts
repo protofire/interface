5fbfa54e7b8bc814b20b6bf56d2ee6983da7149b
Leoni Mella <leoni.mella@gmail.com>
2025-12-11 17:25:14 -0300

    test: update token list url

--- apps/web/src/constants/lists.ts
+++ apps/web/src/constants/lists.ts
@@ -1,4 +1,4 @@
-const LIST = 'https://assets.swap.w3us.site/networks/stable.json'
+const LIST = 'https://assets.swap.w3us.site/networks/stable.json?v=2.0.0'
 
 export const DEFAULT_INACTIVE_LIST_URLS: string[] = []
 export const DEFAULT_ACTIVE_LIST_URLS: string[] = [LIST]
