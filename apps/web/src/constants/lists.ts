6d50ed9cefcd5016c0680c81f11660deb8d3477b
msvstj <savastijonok@gmail.com>
2025-12-22 19:42:27 +0200

    feat: add pyusd0 to common bases and token list

--- apps/web/src/constants/lists.ts
+++ apps/web/src/constants/lists.ts
@@ -1,4 +1,4 @@
-const DEFAULT_TOKEN_LIST = 'https://assets.swap.w3us.site/networks/flow_v5_1.json'
+const DEFAULT_TOKEN_LIST = 'https://assets.swap.w3us.site/networks/flow.json?v=5.4.1'
 export const DEFAULT_INACTIVE_LIST_URLS: string[] = []
 export const DEFAULT_ACTIVE_LIST_URLS: string[] = [DEFAULT_TOKEN_LIST]
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST]
