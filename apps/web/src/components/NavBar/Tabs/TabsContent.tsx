8e9df34c3dd5c42f7080b6401e9d1d57f5237a19
msvstj <savastijonok@gmail.com>
2025-12-22 20:07:57 +0200

    feat: add external links to header

--- apps/web/src/components/NavBar/Tabs/TabsContent.tsx
+++ apps/web/src/components/NavBar/Tabs/TabsContent.tsx
@@ -1,15 +1,9 @@
-import { CreditCardIcon } from 'components/Icons/CreditCard'
-import { Limit } from 'components/Icons/Limit'
-import { Send } from 'components/Icons/Send'
-import { SwapV2 } from 'components/Icons/SwapV2'
 import { MenuItem } from 'components/NavBar/CompanyMenu/Content'
 import { useTabsVisible } from 'components/NavBar/ScreenSizes'
-import forkConfig from 'forkConfig'
 import { useTheme } from 'lib/styled-components'
 import { useLocation } from 'react-router-dom'
 import { useSwapAndLimitContext } from 'state/swap/useSwapContext'
 import { ExternalLink } from 'ui/src/components/icons'
-import { UNIVERSE_CHAIN_INFO } from 'uniswap/src/constants/chains'
 import { FeatureFlags } from 'uniswap/src/features/gating/flags'
 import { useFeatureFlag } from 'uniswap/src/features/gating/hooks'
 import { useTranslation } from 'uniswap/src/i18n'
@@ -31,19 +25,18 @@ export type TabsItem = MenuItem & {
 }
 
 export const useTabsContent = (props?: { includeNftsLink?: boolean }): TabsSection[] => {
-  const { t } = useTranslation()
-  const forAggregatorEnabled = useFeatureFlag(FeatureFlags.ForAggregator)
-  const { pathname } = useLocation()
-  const theme = useTheme()
-  const areTabsVisible = useTabsVisible()
-  const { chainId: universeChainId } = useSwapAndLimitContext()
-  const chainId = universeChainId ? universeChainId : UniverseChainId.FlowMainnet
   return [
-    // {
-    //   title: t('common.trade'),
-    //   href: '/aggregator',
-    //   isActive: pathname.startsWith('/swap') || pathname.startsWith('/limit') || pathname.startsWith('/send')
-    // },
-
+    {
+      title: 'Liquidity',
+      href: `https://liquidity.flow.com/`,
+      icon: <ExternalLink size="$icon.16" color="$neutral2" />,
+      internal: false,
+    },
+    {
+      title: 'Bridge',
+      href: `https://bridge.flow.com`,
+      icon: <ExternalLink size="$icon.16" color="$neutral2" />,
+      internal: false,
+    },
   ]
 }
