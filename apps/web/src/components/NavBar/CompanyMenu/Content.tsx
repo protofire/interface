7af657e569f77e871d6a10e1190ca4ead07c8ce3
msvstj <savastijonok@gmail.com>
2026-01-21 16:04:46 +0200

    feat: add UI assets page

--- apps/web/src/components/NavBar/CompanyMenu/Content.tsx
+++ apps/web/src/components/NavBar/CompanyMenu/Content.tsx
@@ -22,7 +22,8 @@ export const useMenuContent = (): MenuSection[] => {
     {
       title: 'Help',
       items: [
-        { label: 'Deployed Contracts', href: '/deployments' },
+        { label: 'Deployed Contracts', href: '/deployments', internal: true },
+        { label: 'Brand Kit', href: '/brand-kit', internal: true },
         { label: 'Contact Support', href: 'https://swap-support.protofire.io/' },
       ],
     },
