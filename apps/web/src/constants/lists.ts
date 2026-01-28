f6902189b39357e59043215f733e565dccf4e6cb
Den <dzianis.smalonski@protofire.io>
2026-01-28 09:04:50 +0100

    chore: Update default token list version to 5.4.2.

--- apps/web/src/constants/lists.ts
+++ apps/web/src/constants/lists.ts
@@ -1,4 +1,4 @@
-const DEFAULT_TOKEN_LIST = 'https://assets.swap.w3us.site/networks/flow.json?v=5.4.1'
+const DEFAULT_TOKEN_LIST = 'https://assets.swap.w3us.site/networks/flow.json?v=5.4.2'
 export const DEFAULT_INACTIVE_LIST_URLS: string[] = []
 export const DEFAULT_ACTIVE_LIST_URLS: string[] = [DEFAULT_TOKEN_LIST]
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST]
