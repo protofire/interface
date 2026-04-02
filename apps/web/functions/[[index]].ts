e4d189bceb09b49795978dbcdda905c03930029e
msvstj <savastijonok@gmail.com>
2026-04-02 15:18:51 +0300

    feat: add og thumbnail and description

--- apps/web/functions/[[index]].ts
+++ apps/web/functions/[[index]].ts
@@ -14,7 +14,7 @@ function doesMatchPath(path: string): boolean {
 
 export const onRequest: PagesFunction = async ({ request, next }) => {
   const requestURL = new URL(request.url)
-  const imageUri = requestURL.origin + '/images/1200x630_Rich_Link_Preview_Image.png'
+  const imageUri = requestURL.origin + '/images/1200x630.png'
   const data = {
     title: 'Uniswap Interface',
     image: imageUri,
