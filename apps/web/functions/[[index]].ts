2a670623b863be35a7e1a428f2d9b57ec1c6a583
msvstj <savastijonok@gmail.com>
2026-04-02 18:52:05 +0300

    fix: update description

--- apps/web/functions/[[index]].ts
+++ apps/web/functions/[[index]].ts
@@ -16,10 +16,10 @@ export const onRequest: PagesFunction = async ({ request, next }) => {
   const requestURL = new URL(request.url)
   const imageUri = requestURL.origin + '/images/1200x630.png'
   const data = {
-    title: 'Uniswap Interface',
+    title: 'Swap Interface',
     image: imageUri,
     url: request.url,
-    description: 'Swap or provide liquidity on the Uniswap Protocol',
+    description: 'Swap or provide liquidity on the Stable Network',
   }
   const response = next()
   if (doesMatchPath(requestURL.pathname)) {
