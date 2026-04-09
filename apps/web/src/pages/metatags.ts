dd86620c75b1cd7690682eb2881f14211a531f52
msvstj <savastijonok@gmail.com>
2026-04-09 15:25:49 +0300

    fix: metatags url

--- apps/web/src/pages/metatags.ts
+++ apps/web/src/pages/metatags.ts
@@ -7,7 +7,7 @@ const DEFAULT_METATAGS: MetaTagInjectorInput = {
   title: t('interface.metatags.title'),
   description: t`interface.metatags.description`,
   image: `https://shapeswap.xyz/images/1200x630.png`,
-  url: 'https://shapeswap.xyz/images/1200x630.png',
+  url: 'https://shapeswap.xyz',
 }
 
 type MetatagAttributes = { property?: string; name?: string; content: string }
